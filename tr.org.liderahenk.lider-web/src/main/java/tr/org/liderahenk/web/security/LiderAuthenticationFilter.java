236f46c3031356183ba80927e88173dce77ce736
emre.akkaya <emre.akkaya@agem.com.tr>
2016-02-02 17:45:20 +0200

    pom fix

--- tr.org.liderahenk.lider-web/src/main/java/tr/org/liderahenk/web/security/LiderAuthenticationFilter.java
+++ tr.org.liderahenk.lider-web/src/main/java/tr/org/liderahenk/web/security/LiderAuthenticationFilter.java
@@ -2,12 +2,12 @@ package tr.org.liderahenk.web.security;
 
 import javax.servlet.ServletRequest;
 import javax.servlet.ServletResponse;
+// TODO
+//import org.apache.shiro.web.filter.authc.AuthenticationFilter;
 
-import org.apache.shiro.web.filter.authc.AuthenticationFilter;
+public class LiderAuthenticationFilter /*extends AuthenticationFilter */{
 
-public class LiderAuthenticationFilter extends AuthenticationFilter{
-
-	@Override
+//	@Override
 	protected boolean onAccessDenied(ServletRequest arg0, ServletResponse arg1) throws Exception {
 		// TODO Auto-generated method stub
 		return true;
