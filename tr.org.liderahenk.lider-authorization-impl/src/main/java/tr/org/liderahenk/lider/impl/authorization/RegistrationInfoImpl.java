c94a85cdb8ea829dd865b43a7ecdd2ee80c93a83
emrekgn <emrekaganakkaya@gmail.com>
2016-02-19 10:47:54 +0200

    agent registration service updated - removed owner attr

--- tr.org.liderahenk.lider-authorization-impl/src/main/java/tr/org/liderahenk/lider/impl/authorization/RegistrationInfoImpl.java
+++ tr.org.liderahenk.lider-authorization-impl/src/main/java/tr/org/liderahenk/lider/impl/authorization/RegistrationInfoImpl.java
@@ -3,29 +3,23 @@ package tr.org.liderahenk.lider.impl.authorization;
 import tr.org.liderahenk.lider.core.api.auth.IRegistrationInfo;
 import tr.org.liderahenk.lider.core.api.auth.RegistrationStatus;
 
-
 /**
  * Default implementation of {@link IRegistrationInfo}.
- *  
+ * 
  * @author <a href="mailto:birkan.duman@gmail.com">Birkan Duman</a>
  *
  */
 public class RegistrationInfoImpl implements IRegistrationInfo {
 
-	
 	private String message;
 	private String XMPPServer;
 	private String XMPPDomain;
 	private RegistrationStatus status;
 	private String agentJid;
 	private String agentDn;
-	
-	public RegistrationInfoImpl(RegistrationStatus status, 
-			String agentJid,
-			String agentDn,
-			 String xmppServer,
-			 String xmppDomain,
-			 String message) {
+
+	public RegistrationInfoImpl(RegistrationStatus status, String agentJid, String agentDn, String xmppServer,
+			String xmppDomain, String message) {
 		this.agentJid = agentJid;
 		this.agentDn = agentDn;
 		this.status = status;
@@ -38,7 +32,7 @@ public class RegistrationInfoImpl implements IRegistrationInfo {
 	public RegistrationStatus getStatus() {
 		return status;
 	}
-	
+
 	public void setStatus(RegistrationStatus status) {
 		this.status = status;
 	}
@@ -47,17 +41,16 @@ public class RegistrationInfoImpl implements IRegistrationInfo {
 	public String getXmppServer() {
 		return XMPPServer;
 	}
-	
+
 	public void setXMPPServer(String xMPPServer) {
 		XMPPServer = xMPPServer;
 	}
-
 
 	@Override
 	public String getMessage() {
 		return message;
 	}
-	
+
 	public void setMessage(String message) {
 		this.message = message;
 	}
@@ -66,7 +59,7 @@ public class RegistrationInfoImpl implements IRegistrationInfo {
 	public String getAgentJid() {
 		return agentJid;
 	}
-	
+
 	public void setAgentJid(String agentJid) {
 		this.agentJid = agentJid;
 	}
@@ -83,7 +76,7 @@ public class RegistrationInfoImpl implements IRegistrationInfo {
 	public String getAgentDn() {
 		return agentDn;
 	}
-	
+
 	public void setAgentDn(String agentDn) {
 		this.agentDn = agentDn;
 	}
--- tr.org.liderahenk.lider-authorization-impl/src/main/java/tr/org/liderahenk/lider/impl/authorization/RegistrationServiceImpl.java
+++ tr.org.liderahenk.lider-authorization-impl/src/main/java/tr/org/liderahenk/lider/impl/authorization/RegistrationServiceImpl.java
@@ -20,54 +20,48 @@ import tr.org.liderahenk.lider.core.model.ldap.LdapEntry;
  * Default implementation of {@link IRegistrationService}.
  * 
  * @author <a href="mailto:birkan.duman@gmail.com">Birkan Duman</a>
+ * @author <a href="mailto:emre.akkaya@agem.com.tr">Emre Akkaya</a>
  *
  */
 public class RegistrationServiceImpl implements IRegistrationService {
 
-	private static Logger log = LoggerFactory.getLogger(RegistrationServiceImpl.class);
+	private static Logger logger = LoggerFactory.getLogger(RegistrationServiceImpl.class);
 
 	private IClientIdMatcher idMatcher;
 	private ILDAPService ldapService;
 
-	public void setIdMatcher(IClientIdMatcher idMatcher) {
-		this.idMatcher = idMatcher;
-	}
-
-	public void setLdapService(ILDAPService ldapService) {
-		this.ldapService = ldapService;
-	}
-
 	@Override
-	public IRegistrationInfo register(String identifier, String member, String generatedPassword,
-			Boolean useExistingEntry, String remoteHost, String remoteIP) throws Exception {
+	public IRegistrationInfo register(String identifier, String generatedPassword, Boolean useExistingEntry,
+			String remoteHost, String remoteIP) throws Exception {
 
 		Map<String, String[]> entryAttributes = idMatcher.match(identifier);
-		String entryDN = entryAttributes.get("DN")[0];// (String)
-														// idMatcher.match(identifier);
+		String entryDN = entryAttributes.get("DN")[0];
 		String[] attrs = { "uid" };
 		final List<LdapEntry> entry = ldapService.search(idMatcher.getAgentJidAttribute(), identifier, attrs);
 
 		if (!entry.isEmpty()) {
 			if (!useExistingEntry) {
+				logger.debug(
+						"Agent DN {} already exists! Set useExistingEntry flag to true to retrieve & use that information.",
+						entry.get(0).getDistinguishedName());
 				return new RegistrationInfoImpl(RegistrationStatus.ALREADY_EXISTS, null, null, null, null, entry.get(0)
 						.getDistinguishedName()
-						+ " already exists! Set useExistingEntry flag to true to retrieve & use that information");
+						+ " already exists! Set useExistingEntry flag to true to retrieve & use that information.");
 			} else {
 				ldapService.updateEntry(entry.get(0).getDistinguishedName(), "userPassword", generatedPassword);
+				logger.debug(
+						"Agent DN {} already exists! Updated its password with the one you submitted and returning existing entry's attributes.",
+						entry.get(0).getDistinguishedName());
 				return new RegistrationInfoImpl(RegistrationStatus.PASSWORD_UPDATED,
 						entry.get(0).getAttributes().get(idMatcher.getAgentJidAttribute()).toString(),
-						entry.get(0).getDistinguishedName(),
-						// entry.getAttributes().get("userPassword"),
-						idMatcher.getXmppServer(), idMatcher.getXmppDomain(),
-						// "http://localhost:8181/",
+						entry.get(0).getDistinguishedName(), idMatcher.getXmppServer(), idMatcher.getXmppDomain(),
 						entry.get(0).getDistinguishedName()
-								+ " already exists! Updated its password with the one you submitted and returning existing entry's attributes");
+								+ " already exists! Updated its password with the one you submitted and returning existing entry's attributes.");
 			}
-		} else /* if( entry == null ) */ {
+		} else {
 			Map<String, String[]> attributes = new HashMap<String, String[]>();
 			attributes.put("objectClass", new String[] { "pardusDevice", "device" });
 			attributes.put("cn", new String[] { identifier });
-			attributes.put("owner", new String[] { member });
 			attributes.put("uid", new String[] { identifier });
 			attributes.put("userPassword", new String[] { generatedPassword });
 
@@ -75,6 +69,7 @@ public class RegistrationServiceImpl implements IRegistrationService {
 				ldapService.addEntry(entryDN, attributes);
 				LdapEntry newEntry = ldapService.getEntry(entryDN,
 						new String[] { idMatcher.getAgentJidAttribute(), "userPassword" });
+				logger.debug("Agent DN {} created successfully!", entryDN);
 				return new RegistrationInfoImpl(RegistrationStatus.REGISTERED,
 						newEntry.getAttributes().get(idMatcher.getAgentJidAttribute()), entryDN,
 						idMatcher.getXmppServer(), idMatcher.getXmppDomain(), entryDN + " created successfully!");
@@ -85,6 +80,14 @@ public class RegistrationServiceImpl implements IRegistrationService {
 			}
 		}
 
+	}
+
+	public void setIdMatcher(IClientIdMatcher idMatcher) {
+		this.idMatcher = idMatcher;
+	}
+
+	public void setLdapService(ILDAPService ldapService) {
+		this.ldapService = ldapService;
 	}
 
 }
