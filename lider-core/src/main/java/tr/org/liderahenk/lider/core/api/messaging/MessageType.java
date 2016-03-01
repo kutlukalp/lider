8b2a65d5b5ffd34195526ff589eece774c9b37f3
emrekgn <emrekaganakkaya@gmail.com>
2016-03-01 18:06:29 +0200

    added login & logout message types for lider

--- lider-core/src/main/java/tr/org/liderahenk/lider/core/api/messaging/MessageType.java
+++ lider-core/src/main/java/tr/org/liderahenk/lider/core/api/messaging/MessageType.java
@@ -1,12 +1,19 @@
 package tr.org.liderahenk.lider.core.api.messaging;
 
 /**
- * Message types used between agents and Lider XMPP client.<br/><br/>
+ * Message types used between agents and Lider XMPP client.<br/>
+ * <br/>
  * 
  * <b>TASK_STATUS</b>: Contains task-related messages.<br/>
- * <b>REGISTER</b>: Indicates that sender (agent) wants to register to the system.<br/>
- * <b>UNREGISTER</b>: Indicates that sender (agent) wants to unregister from the system.<br/>
+ * <b>REGISTER</b>: Indicates that sender (agent) wants to register to the
+ * system.<br/>
+ * <b>UNREGISTER</b>: Indicates that sender (agent) wants to unregister from the
+ * system.<br/>
  * <b>GET_POLICIES</b>: Agent sends this message during user login.<br/>
+ * <b>LOGIN</b>: Agent sends this message for log purposes during user login.
+ * <br/>
+ * <b>LOGOUT</b>: Agent sends this message for log purposes during user logout.
+ * <br/>
  * 
  * @author <a href="mailto:birkan.duman@gmail.com">Birkan Duman</a>
  * @author <a href="mailto:emre.akkaya@agem.com.tr">Emre Akkaya</a>
@@ -14,5 +21,5 @@ package tr.org.liderahenk.lider.core.api.messaging;
  * 
  */
 public enum MessageType {
-	TASK_STATUS, REGISTER, UNREGISTER, GET_POLICIES
+	TASK_STATUS, REGISTER, UNREGISTER, GET_POLICIES, LOGIN, LOGOUT
 }
