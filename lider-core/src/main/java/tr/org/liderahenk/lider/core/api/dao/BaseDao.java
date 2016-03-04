859a7ece996b2aa7b3dbae2c08ad58958743fb41
emrekgn <emrekaganakkaya@gmail.com>
2016-03-04 18:36:42 +0200

    updated base dao interface (work in progress)

--- lider-core/src/main/java/tr/org/liderahenk/lider/core/api/dao/BaseDao.java
+++ lider-core/src/main/java/tr/org/liderahenk/lider/core/api/dao/BaseDao.java
@@ -1,19 +1,36 @@
 package tr.org.liderahenk.lider.core.api.dao;
 
-
 import java.util.List;
 import java.util.Map;
 
+/**
+ * Base DAO interface for all database operations. Other DAO classes should
+ * implement this interface.
+ * 
+ * @author <a href="mailto:emre.akkaya@agem.com.tr">Emre Akkaya</a>
+ *
+ * @param <T>
+ *            any class with {@link javax.persistence.Entity} annotation.
+ */
 public interface BaseDao<T> {
-	
-	T create(T o);
-	void delete(Object id);
-	T find(Object id);
+
+	T save(T o);
+
 	T update(T t);
-	
+
+	T saveOrUpdate(T t);
+
+	void delete(Long id);
+
 	long countAll();
-	List<T> findByProperty(Class<T> obj, String propertyName, Object value, int maxResults);
-	List<T> findByProperties(Class<T> obj, Map<String, Object> propertiesMap, List<PropertyOrder> orders, Integer maxResults);
+
+	T find(Long id);
+
 	List<? extends T> findAll(Class<? extends T> obj, int maxResults);
-	
+
+	List<? extends T> findByProperty(Class<? extends T> obj, String propertyName, Object value, int maxResults);
+
+	List<? extends T> findByProperties(Class<? extends T> obj, Map<String, Object> propertiesMap,
+			List<PropertyOrder> orders, Integer maxResults);
+
 }
