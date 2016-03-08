e9b9c7e9ba3c369e864468dd8bf33ce65571c838
emrekgn <emrekaganakkaya@gmail.com>
2016-03-08 10:55:57 +0200

    added common constants

---
+++ lider-core/src/main/java/tr/org/liderahenk/lider/core/api/constants/LiderConstants.java
@@ -0,0 +1,15 @@
+package tr.org.liderahenk.lider.core.api.constants;
+
+/**
+ * Provides common constants used throughout the system.
+ * 
+ * @author <a href="mailto:emre.akkaya@agem.com.tr">Emre Akkaya</a>
+ *
+ */
+public class LiderConstants {
+
+	public static final class EVENTS {
+		public static final String FILE_RECEIVED = "tr/org/liderahenk/file/received";
+	}
+
+}
