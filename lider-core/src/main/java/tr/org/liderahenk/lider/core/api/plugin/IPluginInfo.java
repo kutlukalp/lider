aa8eee5af4a3fefc959ea5c11a0b0b4182a600aa
emrekgn <emrekaganakkaya@gmail.com>
2016-03-16 16:08:19 +0200

    impl new bundle for plugin management/registration

--- lider-core/src/main/java/tr/org/liderahenk/lider/core/api/plugin/IPluginInfo.java
+++ lider-core/src/main/java/tr/org/liderahenk/lider/core/api/plugin/IPluginInfo.java
@@ -8,16 +8,40 @@ package tr.org.liderahenk.lider.core.api.plugin;
  */
 public interface IPluginInfo {
 
+	/**
+	 * 
+	 * @return plugin name
+	 */
 	String getPluginName();
 
+	/**
+	 * 
+	 * @return plugin version
+	 */
 	String getPluginVersion();
 
+	/**
+	 * 
+	 * @return description
+	 */
 	String getDescription();
 
+	/**
+	 * 
+	 * @return true if profiles of this plugin can be executed for machines.
+	 */
 	boolean isMachineOriented();
 
+	/**
+	 * 
+	 * @return true if profiles of this plugin can be executed for users.
+	 */
 	boolean isUserOriented();
 
+	/**
+	 * 
+	 * @return true if profiles of this plugin can be used in a policy.
+	 */
 	boolean isPolicyPlugin();
 
 }
