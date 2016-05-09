253f047ed4595cdfd1967a149417b9cd573d6ec5
emrekgn <emrekaganakkaya@gmail.com>
2016-05-09 18:05:42 +0300

    impl controller & processor for report CRUD operations

---
+++ lider-rest-impl/src/main/java/tr/org/liderahenk/lider/rest/ReportRequestProcessorImpl.java
@@ -0,0 +1,134 @@
+package tr.org.liderahenk.lider.rest;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import org.codehaus.jackson.map.ObjectMapper;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import tr.org.liderahenk.lider.core.api.persistence.dao.IReportDao;
+import tr.org.liderahenk.lider.core.api.persistence.entities.IReportTemplate;
+import tr.org.liderahenk.lider.core.api.persistence.factories.IEntityFactory;
+import tr.org.liderahenk.lider.core.api.rest.IRequestFactory;
+import tr.org.liderahenk.lider.core.api.rest.IResponseFactory;
+import tr.org.liderahenk.lider.core.api.rest.enums.RestResponseStatus;
+import tr.org.liderahenk.lider.core.api.rest.processors.IReportRequestProcessor;
+import tr.org.liderahenk.lider.core.api.rest.requests.IReportTemplateRequest;
+import tr.org.liderahenk.lider.core.api.rest.responses.IRestResponse;
+
+/**
+ * Processor class for handling/processing report data.
+ * 
+ * @author <a href="mailto:emre.akkaya@agem.com.tr">Emre Akkaya</a>
+ *
+ */
+public class ReportRequestProcessorImpl implements IReportRequestProcessor {
+
+	private static Logger logger = LoggerFactory.getLogger(ReportRequestProcessorImpl.class);
+
+	private IReportDao reportDao;
+	private IEntityFactory entityFactory;
+	private IRequestFactory requestFactory;
+	private IResponseFactory responseFactory;
+
+	@Override
+	public IRestResponse add(String json) {
+		try {
+			IReportTemplateRequest request = requestFactory.createReportTemplateRequest(json);
+
+			IReportTemplate template = entityFactory.createReportTemplate(request);
+			template = reportDao.save(template);
+
+			Map<String, Object> resultMap = new HashMap<String, Object>();
+			resultMap.put("template", template.toJson());
+
+			return responseFactory.createResponse(RestResponseStatus.OK, "Record saved.", resultMap);
+		} catch (Exception e) {
+			logger.error(e.getMessage(), e);
+			return responseFactory.createResponse(RestResponseStatus.ERROR, e.getMessage());
+		}
+	}
+
+	@Override
+	public IRestResponse update(String json) {
+		try {
+			IReportTemplateRequest request = requestFactory.createReportTemplateRequest(json);
+			IReportTemplate template = reportDao.find(request.getId());
+
+			template = entityFactory.createReportTemplate(template, request);
+			template = reportDao.saveOrUpdate(template);
+
+			Map<String, Object> resultMap = new HashMap<String, Object>();
+			resultMap.put("template", template.toJson());
+
+			return responseFactory.createResponse(RestResponseStatus.OK, "Record updated.", resultMap);
+		} catch (Exception e) {
+			logger.error(e.getMessage(), e);
+			return responseFactory.createResponse(RestResponseStatus.ERROR, e.getMessage());
+		}
+	}
+
+	@Override
+	public IRestResponse list(String name) {
+		// Build search criteria
+		Map<String, Object> propertiesMap = new HashMap<String, Object>();
+		if (name != null && !name.isEmpty()) {
+			propertiesMap.put("name", name);
+		}
+
+		// Find desired templates
+		List<? extends IReportTemplate> templates = reportDao.findByProperties(IReportTemplate.class, propertiesMap,
+				null, null);
+
+		// Construct result map
+		Map<String, Object> resultMap = new HashMap<String, Object>();
+		ObjectMapper mapper = new ObjectMapper();
+		try {
+			resultMap.put("templates", mapper.writeValueAsString(templates));
+		} catch (Exception e) {
+			logger.error(e.getMessage(), e);
+		}
+
+		return responseFactory.createResponse(RestResponseStatus.OK, "Records listed.", resultMap);
+	}
+
+	@Override
+	public IRestResponse get(Long id) {
+		if (id == null) {
+			throw new IllegalArgumentException("ID was null.");
+		}
+		IReportTemplate template = reportDao.find(new Long(id));
+		Map<String, Object> resultMap = new HashMap<String, Object>();
+		resultMap.put("template", template.toJson());
+		return responseFactory.createResponse(RestResponseStatus.OK, "Record retrieved.", resultMap);
+	}
+
+	@Override
+	public IRestResponse delete(Long id) {
+		if (id == null) {
+			throw new IllegalArgumentException("ID was null.");
+		}
+		reportDao.delete(new Long(id));
+		logger.info("Report template record deleted: {}", id);
+		return responseFactory.createResponse(RestResponseStatus.OK, "Record deleted.");
+	}
+
+	public void setReportDao(IReportDao reportDao) {
+		this.reportDao = reportDao;
+	}
+
+	public void setEntityFactory(IEntityFactory entityFactory) {
+		this.entityFactory = entityFactory;
+	}
+
+	public void setRequestFactory(IRequestFactory requestFactory) {
+		this.requestFactory = requestFactory;
+	}
+
+	public void setResponseFactory(IResponseFactory responseFactory) {
+		this.responseFactory = responseFactory;
+	}
+
+}
---
+++ lider-web/src/main/java/tr/org/liderahenk/web/controller/ReportController.java
@@ -0,0 +1,150 @@
+package tr.org.liderahenk.web.controller;
+
+import java.io.UnsupportedEncodingException;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import tr.org.liderahenk.lider.core.api.rest.IResponseFactory;
+import tr.org.liderahenk.lider.core.api.rest.processors.IReportRequestProcessor;
+import tr.org.liderahenk.lider.core.api.rest.responses.IRestResponse;
+import tr.org.liderahenk.web.controller.utils.ControllerUtils;
+
+/**
+ * Controller for report related operations.
+ * 
+ * @author <a href="mailto:emre.akkaya@agem.com.tr">Emre Akkaya</a>
+ *
+ */
+@Controller
+@RequestMapping("/lider/report")
+public class ReportController {
+
+	private static Logger logger = LoggerFactory.getLogger(ReportController.class);
+
+	@Autowired
+	private IResponseFactory responseFactory;
+	@Autowired
+	private IReportRequestProcessor reportProcessor;
+
+	// TODO validate template action
+
+	// TODO generate report action
+
+	/**
+	 * Create new template.
+	 * 
+	 * @param requestBody
+	 * @param request
+	 * @return
+	 * @throws UnsupportedEncodingException
+	 */
+	@RequestMapping(value = "/add", method = { RequestMethod.GET, RequestMethod.POST })
+	@ResponseBody
+	public IRestResponse addTemplate(@RequestBody String requestBody, HttpServletRequest request)
+			throws UnsupportedEncodingException {
+		String requestBodyDecoded = ControllerUtils.decodeRequestBody(requestBody);
+		logger.info("Request received. URL: '/lider/report/add' Body: {}", requestBodyDecoded);
+		IRestResponse restResponse = reportProcessor.add(requestBodyDecoded);
+		logger.info("Completed processing request, returning result: {}", restResponse.toJson());
+		return restResponse;
+	}
+
+	/**
+	 * Update given template.
+	 * 
+	 * @param requestBody
+	 * @param request
+	 * @return
+	 * @throws UnsupportedEncodingException
+	 */
+	@RequestMapping(value = "/update", method = { RequestMethod.GET, RequestMethod.POST })
+	@ResponseBody
+	public IRestResponse updateTemplate(@RequestBody String requestBody, HttpServletRequest request)
+			throws UnsupportedEncodingException {
+		String requestBodyDecoded = ControllerUtils.decodeRequestBody(requestBody);
+		logger.info("Request received. URL: '/lider/report/update' Body: {}", requestBodyDecoded);
+		IRestResponse restResponse = reportProcessor.update(requestBodyDecoded);
+		logger.info("Completed processing request, returning result: {}", restResponse.toJson());
+		return restResponse;
+	}
+
+	/**
+	 * List policies according to given parameters.
+	 * 
+	 * @param label
+	 * @param active
+	 * @param request
+	 * @return
+	 * @throws UnsupportedEncodingException
+	 */
+	@RequestMapping(value = "/list", method = { RequestMethod.GET, RequestMethod.POST })
+	@ResponseBody
+	public IRestResponse listPolicies(@RequestParam(value = "name", required = false) String name,
+			HttpServletRequest request) throws UnsupportedEncodingException {
+		logger.info("Request received. URL: '/lider/report/list?name={}'", name);
+		IRestResponse restResponse = reportProcessor.list(name);
+		logger.info("Completed processing request, returning result: {}", restResponse.toJson());
+		return restResponse;
+	}
+
+	/**
+	 * Retrieve template specified by id
+	 * 
+	 * @param id
+	 * @param request
+	 * @return
+	 * @throws UnsupportedEncodingException
+	 */
+	@RequestMapping(value = "/{id:[\\d]+}/get", method = { RequestMethod.GET, RequestMethod.POST })
+	@ResponseBody
+	public IRestResponse getTemplate(@PathVariable final long id, HttpServletRequest request)
+			throws UnsupportedEncodingException {
+		logger.info("Request received. URL: '/lider/report/{}/get'", id);
+		IRestResponse restResponse = reportProcessor.get(id);
+		logger.info("Completed processing request, returning result: {}", restResponse.toJson());
+		return restResponse;
+	}
+
+	/**
+	 * Delete template specified by id.
+	 * 
+	 * @param id
+	 * @param request
+	 * @return
+	 * @throws UnsupportedEncodingException
+	 */
+	@RequestMapping(value = "/{id:[\\d]+}/delete", method = { RequestMethod.GET, RequestMethod.POST })
+	@ResponseBody
+	public IRestResponse deleteTemplate(@PathVariable final long id, HttpServletRequest request)
+			throws UnsupportedEncodingException {
+		logger.info("Request received. URL: '/lider/report/{}/delete'", id);
+		IRestResponse restResponse = reportProcessor.delete(id);
+		logger.info("Completed processing request, returning result: {}", restResponse.toJson());
+		return restResponse;
+	}
+
+	/**
+	 * Handle predefined exceptions that we did not write and did not throw.
+	 * 
+	 * @param e
+	 * @return IRestResponse instance which holds exception message with ERROR
+	 *         status
+	 */
+	@ExceptionHandler(Exception.class)
+	public IRestResponse handleAllException(Exception e) {
+		return ControllerUtils.handleAllException(e, responseFactory);
+	}
+
+}
