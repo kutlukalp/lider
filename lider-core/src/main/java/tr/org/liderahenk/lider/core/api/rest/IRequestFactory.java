02f7bf062d2c3d4231f9700da030bc8769577b6d
emrekgn <emrekaganakkaya@gmail.com>
2016-05-09 18:03:45 +0300

    add factory methods for new requests

--- lider-core/src/main/java/tr/org/liderahenk/lider/core/api/rest/IRequestFactory.java
+++ lider-core/src/main/java/tr/org/liderahenk/lider/core/api/rest/IRequestFactory.java
@@ -3,8 +3,16 @@ package tr.org.liderahenk.lider.core.api.rest;
 import tr.org.liderahenk.lider.core.api.rest.requests.IPolicyExecutionRequest;
 import tr.org.liderahenk.lider.core.api.rest.requests.IPolicyRequest;
 import tr.org.liderahenk.lider.core.api.rest.requests.IProfileRequest;
+import tr.org.liderahenk.lider.core.api.rest.requests.IReportTemplateRequest;
 import tr.org.liderahenk.lider.core.api.rest.requests.ITaskRequest;
 
+/**
+ * Interface for request factory. Request factories are used to create request
+ * objects from given JSON string.
+ * 
+ * @author <a href="mailto:emre.akkaya@agem.com.tr">Emre Akkaya</a>
+ *
+ */
 public interface IRequestFactory {
 
 	IProfileRequest createProfileRequest(String json) throws Exception;
@@ -14,5 +22,7 @@ public interface IRequestFactory {
 	ITaskRequest createTaskCommandRequest(String json) throws Exception;
 
 	IPolicyExecutionRequest createPolicyExecutionRequest(String json) throws Exception;
+
+	IReportTemplateRequest createReportTemplateRequest(String json) throws Exception;
 
 }
--- lider-service-impl/src/main/java/tr/org/liderahenk/lider/service/RequestFactoryImpl.java
+++ lider-service-impl/src/main/java/tr/org/liderahenk/lider/service/RequestFactoryImpl.java
@@ -8,10 +8,12 @@ import tr.org.liderahenk.lider.core.api.rest.IRequestFactory;
 import tr.org.liderahenk.lider.core.api.rest.requests.IPolicyExecutionRequest;
 import tr.org.liderahenk.lider.core.api.rest.requests.IPolicyRequest;
 import tr.org.liderahenk.lider.core.api.rest.requests.IProfileRequest;
+import tr.org.liderahenk.lider.core.api.rest.requests.IReportTemplateRequest;
 import tr.org.liderahenk.lider.core.api.rest.requests.ITaskRequest;
 import tr.org.liderahenk.lider.service.requests.PolicyExecutionRequestImpl;
 import tr.org.liderahenk.lider.service.requests.PolicyRequestImpl;
 import tr.org.liderahenk.lider.service.requests.ProfileRequestImpl;
+import tr.org.liderahenk.lider.service.requests.ReportTemplateRequestImpl;
 import tr.org.liderahenk.lider.service.requests.TaskRequestImpl;
 
 /**
@@ -27,29 +29,31 @@ public class RequestFactoryImpl implements IRequestFactory {
 	@Override
 	public IProfileRequest createProfileRequest(String json) throws Exception {
 		logger.debug("Creating ProfileRequestImpl instance from json: {}", json);
-		ObjectMapper mapper = new ObjectMapper();
-		return mapper.readValue(json, ProfileRequestImpl.class);
+		return new ObjectMapper().readValue(json, ProfileRequestImpl.class);
 	}
 
 	@Override
 	public IPolicyRequest createPolicyRequest(String json) throws Exception {
 		logger.debug("Creating ProfileRequestImpl instance from json: {}", json);
-		ObjectMapper mapper = new ObjectMapper();
-		return mapper.readValue(json, PolicyRequestImpl.class);
+		return new ObjectMapper().readValue(json, PolicyRequestImpl.class);
 	}
 
 	@Override
 	public IPolicyExecutionRequest createPolicyExecutionRequest(String json) throws Exception {
 		logger.debug("Creating PolicyExecutionImpl instance from json: {}", json);
-		ObjectMapper mapper = new ObjectMapper();
-		return mapper.readValue(json, PolicyExecutionRequestImpl.class);
+		return new ObjectMapper().readValue(json, PolicyExecutionRequestImpl.class);
 	}
 
 	@Override
 	public ITaskRequest createTaskCommandRequest(String json) throws Exception {
 		logger.debug("Creating TaskRequestImpl instance from json: {}", json);
-		ObjectMapper mapper = new ObjectMapper();
-		return mapper.readValue(json, TaskRequestImpl.class);
+		return new ObjectMapper().readValue(json, TaskRequestImpl.class);
+	}
+
+	@Override
+	public IReportTemplateRequest createReportTemplateRequest(String json) throws Exception {
+		logger.debug("Creating ReportTemplateRequestImpl instance from json: {}", json);
+		return new ObjectMapper().readValue(json, ReportTemplateRequestImpl.class);
 	}
 
 }
