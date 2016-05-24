24e93f650443412245442ee6d7b8ecf936e35b5c
İsmail Başaran <basaran.ismaill@gmail.com>
2016-05-24 13:15:25 +0300

    hot deployment listener impl

--- lider-deployer/src/main/java/tr/org/liderahenk/deployer/LiderHotDeployListener.java
+++ lider-deployer/src/main/java/tr/org/liderahenk/deployer/LiderHotDeployListener.java
@@ -1,21 +1,138 @@
 package tr.org.liderahenk.deployer;
 
-import java.nio.file.Path;
+import static java.nio.file.StandardWatchEventKinds.ENTRY_CREATE;
+import static java.nio.file.StandardWatchEventKinds.ENTRY_DELETE;
+import static java.nio.file.StandardWatchEventKinds.ENTRY_MODIFY;
+import static java.nio.file.StandardWatchEventKinds.OVERFLOW;
 
+import java.io.IOException;
+import java.nio.file.FileSystems;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.WatchEvent;
+import java.nio.file.WatchKey;
+import java.nio.file.WatchService;
+import java.util.HashMap;
+import java.util.Map;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import tr.org.liderahenk.lider.core.api.configuration.IConfigurationService;
 import tr.org.liderahenk.lider.core.api.deployer.ILiderHotDeployListener;
 
+/**
+ * Default implementation for {@link ILiderHotDeployListener}
+ * 
+ * @author <a href="mailto:basaran.ismaill@gmail.com">İsmail BAŞARAN</a>
+ * 
+ */
 public class LiderHotDeployListener implements ILiderHotDeployListener{
+	
+	
+	private final static Logger logger = LoggerFactory.getLogger(LiderHotDeployListener.class);
+	
+	private IConfigurationService configurationService;
 
+	private WatchService watcher;
+	private Map<WatchKey,Path> keys;
+	
+	
+	@SuppressWarnings("unchecked")
+    static <T> WatchEvent<T> cast(WatchEvent<?> event) {
+        return (WatchEvent<T>)event;
+    }
+	
+	
 	@Override
 	public void register(Path dir) {
-		// TODO Auto-generated method stub
-		
+		try {
+			WatchKey key = dir.register(watcher, ENTRY_CREATE, ENTRY_DELETE, ENTRY_MODIFY);
+			keys.put(key, dir);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
 	}
-
+	
+	public void init(){
+		try {
+			this.watcher = FileSystems.getDefault().newWatchService();
+			this.keys = new HashMap<WatchKey,Path>();
+			Path dir = Paths.get("/home/ismail/devzone"); // get path value from configuration file
+			register(dir);
+		} catch (IOException e) {
+			logger.error("[LiderHotDeployListener] Exeption occured when initializing hot deployment listener...");
+			e.printStackTrace();
+		} 
+	}
+	
+	public void destroy(){
+		try {
+			if(this.watcher != null){
+				this.watcher.close();
+			}
+		} catch (IOException e) {
+			logger.error("[LiderHotDeployListener] Exeption occured when destroying hot deployment listener...");
+			e.printStackTrace();
+		}
+	}
+	
+	
 	@Override
 	public void processEvents() {
-		// TODO Auto-generated method stub
-		
+		for (;;) {
+
+            // wait for key to be signalled
+            WatchKey key;
+            try {
+                key = watcher.take();
+            } catch (InterruptedException x) {
+                return;
+            }
+
+            Path dir = keys.get(key);
+            if (dir == null) {
+            	logger.error("Watcher key not found...");
+                continue;
+            }
+
+            for (WatchEvent<?> event: key.pollEvents()) {
+                WatchEvent.Kind kind = event.kind();
+
+                if (kind == OVERFLOW) {
+                    continue;
+                }
+
+                // Context for directory entry event is the file name of entry
+                WatchEvent<Path> ev = cast(event);
+                Path name = ev.context();
+                Path child = dir.resolve(name);
+
+                logger.debug("%s: %s\n", event.kind().name(), child);
+
+                
+            }
+            
+            // reset key and remove from set if directory no longer accessible
+            boolean valid = key.reset();
+            if (!valid) {
+                keys.remove(key);
+
+                // all directories are inaccessible
+                if (keys.isEmpty()) {
+                    break;
+                }
+            }
+        }
+	}
+	
+	
+	/**
+	 * 
+	 * @param configurationService
+	 */
+	public void setConfigurationService(IConfigurationService configurationService) {
+		this.configurationService = configurationService;
 	}
 
 }
