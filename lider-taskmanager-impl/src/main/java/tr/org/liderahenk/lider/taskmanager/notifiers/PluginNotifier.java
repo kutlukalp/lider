1bcb0ccd6c2ffd1e9072385e37824795ae3d032e
emrekgn <emrekaganakkaya@gmail.com>
2016-05-26 19:29:08 +0300

    impl plugin & console notifier & also update blueprint accordingly

--- lider-taskmanager-impl/src/main/java/tr/org/liderahenk/lider/taskmanager/notifiers/PluginNotifier.java
+++ lider-taskmanager-impl/src/main/java/tr/org/liderahenk/lider/taskmanager/notifiers/PluginNotifier.java
@@ -1,4 +1,4 @@
-package tr.org.liderahenk.lider.taskmanager;
+package tr.org.liderahenk.lider.taskmanager.notifiers;
 
 import java.util.List;
 
@@ -8,46 +8,41 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import tr.org.liderahenk.lider.core.api.messaging.messages.ITaskStatusMessage;
+import tr.org.liderahenk.lider.core.api.persistence.entities.ICommandExecutionResult;
 import tr.org.liderahenk.lider.core.api.plugin.ITaskAwareCommand;
 
 /**
- * Task status update handler implementation for {@link EventHandler}. This
- * class is responsible for notifying all classes that implemented
+ * Plugin notifier implementation for {@link EventHandler}. This class is
+ * responsible for notifying all classes which implements
  * {@link ITaskAwareCommand} interface.
  * 
- * @author <a href="mailto:birkan.duman@gmail.com">Birkan Duman</a>
  * @author <a href="mailto:emre.akkaya@agem.com.tr">Emre Akkaya</a>
  *
  */
-public class TaskStatusEventHandler implements EventHandler {
+public class PluginNotifier implements EventHandler {
 
-	private static Logger logger = LoggerFactory.getLogger(TaskStatusEventHandler.class);
+	private static Logger logger = LoggerFactory.getLogger(PluginNotifier.class);
 
 	private List<ITaskAwareCommand> taskAwareCommands;
-
-	public void init() {
-		logger.info("Initializing task status handler.");
-	}
+	// TODO improvement. get plugin name, version & command cls id from task
+	// and notify only its related subscriber.
 
 	@Override
 	public void handleEvent(Event event) {
-
-		// TODO improvement. get plugin name, version & command cls id from task
-		// and notify only its related class.
-
-		// TODO improvement. notify asynchronously!
-
 		logger.debug("Started handling task status.");
 
 		ITaskStatusMessage message = (ITaskStatusMessage) event.getProperty("message");
+		ICommandExecutionResult result = (ICommandExecutionResult) event.getProperty("result");
+
 		logger.info("Sending task status message to plugins. Task: {} Status: {}",
 				new Object[] { message.getTaskId(), message.getResponseCode() });
 
-		for (ITaskAwareCommand command : taskAwareCommands) {
+		for (ITaskAwareCommand subscriber : taskAwareCommands) {
 			try {
-				command.onTaskUpdate(message);
-				logger.debug("Notified subscriber: {} command with task status: {}", new Object[] { command, message });
-			} catch (Exception e) {
+				// TODO notify asynchronously!
+				subscriber.onTaskUpdate(result);
+				logger.debug("Notified subscriber: {} with task status: {}", new Object[] { subscriber, message });
+			} catch (Throwable e) {
 				logger.error(e.getMessage(), e);
 			}
 		}
