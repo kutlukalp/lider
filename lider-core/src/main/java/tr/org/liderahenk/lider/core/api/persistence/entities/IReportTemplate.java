41811ba6764a7e20326c2417023d854c13f6e0d5
emrekgn <emrekaganakkaya@gmail.com>
2016-06-02 16:01:23 +0300

    update javadoc

--- lider-core/src/main/java/tr/org/liderahenk/lider/core/api/persistence/entities/IReportTemplate.java
+++ lider-core/src/main/java/tr/org/liderahenk/lider/core/api/persistence/entities/IReportTemplate.java
@@ -17,6 +17,14 @@ public interface IReportTemplate extends IEntity {
 
 	String getDescription();
 
+	/**
+	 * Reporting bundle executes this query and returns its result as a list of
+	 * object array (so make sure to use comma-separated SELECT statement
+	 * instead of simply returning class instance, that way JPA will always
+	 * return an object array).
+	 * 
+	 * @return JPQL statement
+	 */
 	String getQuery();
 
 	Set<? extends IReportTemplateParameter> getTemplateParams();
