8d521c93c44556ba9403230a2c6ff805cc00a87f
emrekgn <emrekaganakkaya@gmail.com>
2016-06-02 16:01:40 +0300

    bug fix for current working dir

--- scripts/build-lider.sh
+++ scripts/build-lider.sh
@@ -6,7 +6,9 @@
 # Generated file can be found under /tmp/lider
 ###
 
-PRJ_ROOT_PATH=$(readlink -f ..)
+pushd $(dirname $0) > /dev/null
+PRJ_ROOT_PATH=$(dirname $(pwd -P))
+popd > /dev/null
 echo "Project path: $PRJ_ROOT_PATH"
 
 # Build project
