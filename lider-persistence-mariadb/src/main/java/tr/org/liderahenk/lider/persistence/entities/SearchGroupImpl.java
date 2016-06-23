ef98f512f899cf566fc14a0e5ecbe3d6d21d0293
emrekgn <emrekaganakkaya@gmail.com>
2016-06-23 15:04:48 +0300

    impl search group entities

--- lider-persistence-mariadb/src/main/java/tr/org/liderahenk/lider/persistence/entities/SearchGroupImpl.java
+++ lider-persistence-mariadb/src/main/java/tr/org/liderahenk/lider/persistence/entities/SearchGroupImpl.java
@@ -1,8 +1,10 @@
 package tr.org.liderahenk.lider.persistence.entities;
 
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
+import java.util.Map;
 
 import javax.persistence.CascadeType;
 import javax.persistence.Column;
@@ -10,10 +12,19 @@ import javax.persistence.Entity;
 import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
+import javax.persistence.Lob;
 import javax.persistence.OneToMany;
 import javax.persistence.Table;
 import javax.persistence.Temporal;
 import javax.persistence.TemporalType;
+import javax.persistence.Transient;
+
+import org.codehaus.jackson.JsonGenerationException;
+import org.codehaus.jackson.JsonParseException;
+import org.codehaus.jackson.annotate.JsonIgnoreProperties;
+import org.codehaus.jackson.map.JsonMappingException;
+import org.codehaus.jackson.map.ObjectMapper;
+import org.codehaus.jackson.type.TypeReference;
 
 import tr.org.liderahenk.lider.core.api.persistence.entities.ISearchGroup;
 import tr.org.liderahenk.lider.core.api.persistence.entities.ISearchGroupEntry;
@@ -27,6 +38,7 @@ import tr.org.liderahenk.lider.core.api.persistence.entities.ISearchGroupEntry;
  * @author <a href="mailto:emre.akkaya@agem.com.tr">Emre Kağan Akkaya</a>
  *
  */
+@JsonIgnoreProperties({ "criteriaBlob" })
 @Entity
 @Table(name = "C_SEARCH_GROUP")
 public class SearchGroupImpl implements ISearchGroup {
@@ -41,8 +53,21 @@ public class SearchGroupImpl implements ISearchGroup {
 	@Column(name = "GROUP_NAME", nullable = false, length = 1000)
 	private String name;
 
-	@Column(name = "SEARCH_CRITERIA", nullable = false, length = 4000)
-	private String searchCriteria;
+	@Column(name = "SEARCH_AGENTS")
+	private boolean searchAgents;
+
+	@Column(name = "SEARCH_USERS")
+	private boolean searchUsers;
+
+	@Column(name = "SEARCH_GROUPS")
+	private boolean searchGroups;
+
+	@Lob
+	@Column(name = "SEARCH_CRITERIA", nullable = true)
+	private byte[] criteriaBlob;
+
+	@Transient
+	private Map<String, String> criteria;
 
 	@Temporal(TemporalType.TIMESTAMP)
 	@Column(name = "CREATE_DATE", nullable = false)
@@ -54,11 +79,14 @@ public class SearchGroupImpl implements ISearchGroup {
 	public SearchGroupImpl() {
 	}
 
-	public SearchGroupImpl(Long id, String name, String searchCriteria, Date createDate,
-			List<SearchGroupEntryImpl> entries) {
+	public SearchGroupImpl(Long id, String name, boolean searchAgents, boolean searchUsers, boolean searchGroups,
+			Map<String, String> criteria, Date createDate, List<SearchGroupEntryImpl> entries) {
 		this.id = id;
 		this.name = name;
-		this.searchCriteria = searchCriteria;
+		this.searchAgents = searchAgents;
+		this.searchUsers = searchUsers;
+		this.searchGroups = searchGroups;
+		setCriteria(criteria);
 		this.createDate = createDate;
 		this.entries = entries;
 	}
@@ -82,12 +110,90 @@ public class SearchGroupImpl implements ISearchGroup {
 	}
 
 	@Override
-	public String getSearchCriteria() {
-		return searchCriteria;
+	public boolean isSearchAgents() {
+		return searchAgents;
 	}
 
-	public void setSearchCriteria(String searchCriteria) {
-		this.searchCriteria = searchCriteria;
+	public void setSearchAgents(boolean searchAgents) {
+		this.searchAgents = searchAgents;
+	}
+
+	@Override
+	public boolean isSearchUsers() {
+		return searchUsers;
+	}
+
+	public void setSearchUsers(boolean searchUsers) {
+		this.searchUsers = searchUsers;
+	}
+
+	@Override
+	public boolean isSearchGroups() {
+		return searchGroups;
+	}
+
+	public void setSearchGroups(boolean searchGroups) {
+		this.searchGroups = searchGroups;
+	}
+
+	@Override
+	public byte[] getCriteriaBlob() {
+		if (criteriaBlob == null && criteria != null) {
+			try {
+				this.criteriaBlob = new ObjectMapper().writeValueAsBytes(criteria);
+			} catch (JsonGenerationException e) {
+				e.printStackTrace();
+			} catch (JsonMappingException e) {
+				e.printStackTrace();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		}
+		return criteriaBlob;
+	}
+
+	public void setCriteriaBlob(byte[] criteriaBlob) {
+		this.criteriaBlob = criteriaBlob;
+		try {
+			this.criteria = new ObjectMapper().readValue(criteriaBlob, new TypeReference<Map<String, Object>>() {
+			});
+		} catch (JsonParseException e) {
+			e.printStackTrace();
+		} catch (JsonMappingException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+
+	@Override
+	public Map<String, String> getCriteria() {
+		if (criteria == null && criteriaBlob != null) {
+			try {
+				this.criteria = new ObjectMapper().readValue(criteriaBlob, new TypeReference<Map<String, Object>>() {
+				});
+			} catch (JsonParseException e) {
+				e.printStackTrace();
+			} catch (JsonMappingException e) {
+				e.printStackTrace();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		}
+		return criteria;
+	}
+
+	public void setCriteria(Map<String, String> criteria) {
+		this.criteria = criteria;
+		try {
+			this.criteriaBlob = new ObjectMapper().writeValueAsBytes(criteria);
+		} catch (JsonGenerationException e) {
+			e.printStackTrace();
+		} catch (JsonMappingException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
 	}
 
 	@Override
@@ -126,7 +232,8 @@ public class SearchGroupImpl implements ISearchGroup {
 
 	@Override
 	public String toString() {
-		return "SearchGroupImpl [id=" + id + ", name=" + name + ", searchCriteria=" + searchCriteria + ", createDate="
+		return "SearchGroupImpl [id=" + id + ", name=" + name + ", searchAgents=" + searchAgents + ", searchUsers="
+				+ searchUsers + ", searchGroups=" + searchGroups + ", criteria=" + criteria + ", createDate="
 				+ createDate + ", entries=" + entries + "]";
 	}
 
