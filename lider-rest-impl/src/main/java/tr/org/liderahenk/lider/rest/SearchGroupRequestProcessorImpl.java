ce18e25834ab089893adf86c5de918a7dc2a47e8
emrekgn <emrekaganakkaya@gmail.com>
2016-06-24 09:45:57 +0300

    impl search group controller & request processor

---
+++ lider-rest-impl/src/main/java/tr/org/liderahenk/lider/rest/SearchGroupRequestProcessorImpl.java
@@ -0,0 +1,38 @@
+package tr.org.liderahenk.lider.rest;
+
+import tr.org.liderahenk.lider.core.api.rest.processors.ISearchGroupRequestProcessor;
+import tr.org.liderahenk.lider.core.api.rest.responses.IRestResponse;
+
+public class SearchGroupRequestProcessorImpl implements ISearchGroupRequestProcessor {
+
+	@Override
+	public IRestResponse add(String json) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public IRestResponse update(String json) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public IRestResponse list(String name) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public IRestResponse get(Long id) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public IRestResponse delete(Long id) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
