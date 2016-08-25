e2e90b867bc7e0849c6e67a45f3867063b461b91
emrekgn <emre.akkaya@agem.com.tr>
2016-08-25 18:41:43 +0300

    move common methods to core utils

---
+++ lider-core/src/main/java/tr/org/liderahenk/lider/core/api/utils/LiderCoreUtils.java
@@ -0,0 +1,21 @@
+package tr.org.liderahenk.lider.core.api.utils;
+
+public class LiderCoreUtils {
+
+	public static boolean isInteger(String s) {
+		if (s.isEmpty())
+			return false;
+		for (int i = 0; i < s.length(); i++) {
+			if (i == 0 && s.charAt(i) == '-') {
+				if (s.length() == 1)
+					return false;
+				else
+					continue;
+			}
+			if (Character.digit(s.charAt(i), 10) < 0)
+				return false;
+		}
+		return true;
+	}
+
+}
